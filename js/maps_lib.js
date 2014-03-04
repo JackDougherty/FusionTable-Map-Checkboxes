6ea901b09f22e268f777105d461a7fdaa5444f8f
Jack Dougherty <jack.dougherty@trincoll.edu>
2014-03-04 11:29:51 -0500

    added TypeNum and Program

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -88,15 +88,8 @@ var MapsLib = {
     var whereClause = MapsLib.locationColumn + " not equal to ''";
 
   //-----custom filters-------
-/*    var type_column = "'TypeNum'";
-    var searchType = type_column + " IN (-1,";
-    if ( $("#cbType1").is(':checked')) searchType += "1,";
-    if ( $("#cbType2").is(':checked')) searchType += "2,";
-    if ( $("#cbType3").is(':checked')) searchType += "3,";
-    if ( $("#cbType4").is(':checked')) searchType += "4,";
-    if ( $("#cbType5").is(':checked')) searchType += "5,";
-    whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";*/
-    //-----Filter by String values did not work properly below
+
+    //---Filter by text, matched with column header and values in your Google Fusion Table
     var type_column = "'Program Type'";
     var tempWhereClause = [];
     if ( $("#cbType1").is(':checked')) tempWhereClause.push("Preschool");
@@ -105,6 +98,16 @@ var MapsLib = {
     if ( $("#cbType4").is(':checked')) tempWhereClause.push("Elementary");
     if ( $("#cbType5").is(':checked')) tempWhereClause.push("High School");
     whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
+
+    //-- To filter by numbers, remove commenting and use code below:
+    /*    var type_column = "'TypeNum'";
+    var searchType = type_column + " IN (-1,";
+    if ( $("#cbType1").is(':checked')) searchType += "1,";
+    if ( $("#cbType2").is(':checked')) searchType += "2,";
+    if ( $("#cbType3").is(':checked')) searchType += "3,";
+    if ( $("#cbType4").is(':checked')) searchType += "4,";
+    if ( $("#cbType5").is(':checked')) searchType += "5,";
+    whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";*/
 
     //-------end of custom filters--------
 
